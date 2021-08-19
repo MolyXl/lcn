39b0c466ff3e37ebf990d329593a06548fbc523a
MaZhuli <345957335@qq.com>
2021-08-19 19:53:56 +0800

    init

---
+++ txlcn-tc/src/main/java/com/codingapi/txlcn/tc/annotation/LcnTransaction.java
@@ -0,0 +1,12 @@
+package com.codingapi.txlcn.tc.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+public @interface LcnTransaction {
+
+
+}
